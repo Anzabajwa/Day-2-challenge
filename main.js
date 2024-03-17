ec134b25325df6ae90cf0c06a76b89287e8272dc
ANZA BAJWA <161083667+Anzabajwa@users.noreply.github.com>
2024-03-17 07:36:48 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("Albert Einstein once said ,\"A person who never made a mistake never tried anything new.\"");
+var famousPerson = "Albert Einstein";
+var Massage = " once said, “A person who never made a mistake never tried anything new.”";
+console.log(famousPerson + Massage);
+var whitespaceName = "\n\t Anza Sarwar \t\n";
+console.log(whitespaceName);
+var withoutwhitespaceName = whitespaceName.trim();
+console.log(withoutwhitespaceName);
---
+++ main.ts
@@ -0,0 +1,15 @@
+console.log("Albert Einstein once said ,\"A person who never made a mistake never tried anything new.\"");
+
+
+
+
+
+let famousPerson : string= "Albert Einstein" ;
+let Massage = " once said, “A person who never made a mistake never tried anything new.”";
+console.log(famousPerson + Massage) ; 
+
+
+let whitespaceName : string = "\n\t Anza Sarwar \t\n"
+console.log(whitespaceName)
+let withoutwhitespaceName = whitespaceName.trim();
+console.log(withoutwhitespaceName)
